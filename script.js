451320a958b967b45ae8f70c74963b3ae3544fde
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-27 19:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-
 function convertToRoman(num) {
   const obj = {
     0: ['M', 1000],
-    1: ['CM', 900],
-    2: ['D', 500],
-    3: ['CD', 400],
-    4: ['C', 100],
-    5: ['XC', 90],
-    6: ['L', 50],
-    7: ['XL', 40],
-    8: ['X', 10],
-    9: ['IX', 9],
-    10: ['V', 5],
-    11: ['IV', 4],
-    12: ['I', 1]
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
   };
 
-  let romanNumeral = '';
+  if (num < 1 || num > 100000) {
+    return "Number out of range";
+  }
 
-  for (let i = 0; i < Object.keys(obj).length; i++) {
-    const key = Object.keys(obj)[i];
-    const value = obj[key];
-    const symbol = value[0];
-    const weight = value[1];
+  let romanNumeral = "";
 
-    while (num >= weight) {
-      romanNumeral += symbol;
-      num -= weight;
+  for (let key in obj) {
+    while (num >= obj[key][1]) {
+      romanNumeral += obj[key][0];
+      num -= obj[key][1];
+    }
+
+    // Handle subtractive notation (e.g., 4 as IV or 9 as IX)
+    if (key % 2 === 0) {
+      let nextKey = Number(key) + 2;
+      if (nextKey < 7 && num >= obj[key][1] - obj[nextKey][1]) {
+        romanNumeral += obj[nextKey][0] + obj[key][0];
+        num -= obj[key][1] - obj[nextKey][1];
+      }
     }
   }
 
   return romanNumeral;
 }
 
-console.log(convertToRoman(36));
+// Test cases
+console.log(convertToRoman(36)); // Output: XXXVI
+
+module.exports = convertToRoman;
